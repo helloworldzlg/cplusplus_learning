e69a72165d3b7a200b136ef7cb4792c69812c1a4
helloworldzlg <helloworld_zlg@outlook.com>
2016-03-13 16:50:27 +0800

    C++回顾 typeid的使用场景

---
+++ typeid的使用场景2.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <typeinfo>
+
+using namespace std; 
+
+class base
+{
+public :
+    virtual void m(){cout<<"base"<<endl;}
+};
+
+class derived : public base
+{
+public:
+    void m(){cout<<"derived"<<endl;}
+};
+
+int main()
+{
+	base* p = new derived;
+	
+	if (typeid(p) == typeid(base*))
+	{
+		cout << "p == base*" << endl;
+	}
+	
+	if (typeid(p) == typeid(derived*))
+	{
+		cout << "p == derived*" << endl;
+	}
+	
+	if (typeid(*p) == typeid(base))
+	{
+		cout << "*p == base" << endl;
+	}
+	
+	if (typeid(*p) == typeid(derived))
+	{
+		cout << "*p == derived" << endl;
+	}	
+	
+	return 0;
+}
